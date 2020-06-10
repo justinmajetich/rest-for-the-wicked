630fc736e25bde5e631bbd7d991bc22a8bbc9c9a
Gzoref <gzoref@gmail.com>
2020-06-10 13:48:13 -0400

    [UPDATE] rest_framwork routers and viewSet

--- rest_for_the_wicked_backend/rest_for_the_wicked_backend/urls.py
+++ rest_for_the_wicked_backend/rest_for_the_wicked_backend/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
+from backend import views
+
+router = routers.DefaultRouter()
+router.register(r'poi', views.PoiViewSet)
+router.register(r'item', views.ItemViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('backend.urls'))
+    path('', include('backend.urls')),
+     path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]

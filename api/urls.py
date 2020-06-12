428738bb7718b157f743ce242a0b7a99a4b1c194
Justin Majetich <justinmajetich@gmail.com>
2020-06-11 22:58:58 -0400

    Configure CORS settings to allow same origin requests

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from rest_framework import routers
 
 urlpatterns = [
-    #path('poi/', views.poi, name='poi-home'),
-    #path('items/', views.items, name='poi-item'),
+    # path('poi/', views.poi, name='poi-home'),
+    # path('items/', views.items, name='poi-item'),
 ]

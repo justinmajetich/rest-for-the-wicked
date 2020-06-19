96541a8a8df3913176798209a2f0629c60541fab
Gzoref <gzoref@gmail.com>
2020-06-19 17:45:27 -0400

    doc: add comments

--- api/models.py
+++ api/models.py
@@ -2,22 +2,50 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 class POI(models.Model):
     '''
     Point of Interest class
     '''
-    name = models.CharField(max_length=100, primary_key=True, unique=True) # name of point of interest. Is a Primary Key
-    description = models.TextField() # POI description
-    parent = models.ForeignKey('POI',  to_field='name', on_delete=models.SET_NULL, blank=True, null=True, related_name='children') # POI parent. Has related name of 'child'.
-    needs_key = models.ForeignKey('Item',  to_field='name', on_delete=models.SET_NULL, blank=True, null=True) # Does POI require a key or not.
-    full_path = models.CharField(max_length=100) # the full url/path
+
+    # name of point of interest. Is a Primary Key
+    name = models.CharField(max_length=100, primary_key=True, unique=True)
+
+    # POI description
+    description = models.TextField()
+
+    # POI parent. Has related name of 'child'.
+    parent = models.ForeignKey('POI', to_field='name',
+                               on_delete=models.SET_NULL,
+                               blank=True, null=True, related_name='children')
+
+    # Does POI require a key or not.
+    needs_key = models.ForeignKey(
+        'Item', to_field='name', on_delete=models.SET_NULL,
+        blank=True, null=True)
+
+    # The full url/path
+    full_path = models.CharField(max_length=100)
+
 
 class Item(models.Model):
     '''
     Item class
     '''
-    name = models.CharField(max_length=100, primary_key=True, unique=True) # name of Item. Is primary key
-    description = models.TextField() # Item description
-    spawn_poi = models.ForeignKey(POI, to_field='name', null=True, on_delete=models.SET_NULL, related_name='spawned_items') # What POI is this item spawned from? Is a Foreign Key
-    use_poi = models.ForeignKey(POI,  to_field='name', blank=True, null=True, on_delete=models.SET_NULL, related_name='usable_items') # What POI uses this Item? Is a Foreign Key
-    is_key = models.BooleanField(default=False) # Is the item a key?
+    # Name of Item. Is primary key
+    name = models.CharField(max_length=100, primary_key=True,
+                            unique=True)
+    # Item description
+    description = models.TextField()
+
+    # What POI is this item spawned from? Is a Foreign Key
+    spawn_poi = models.ForeignKey(POI, to_field='name', null=True,
+                                  on_delete=models.SET_NULL,
+                                  related_name='spawned_items')
+
+    # What POI uses this Item? Is a Foreign Key
+    use_poi = models.ForeignKey(POI, to_field='name', blank=True, null=True,
+                                on_delete=models.SET_NULL,
+                                related_name='usable_items')
+    # Is the item a key
+    is_key = models.BooleanField(default=False)

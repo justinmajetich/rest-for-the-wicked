f90985505b11c70f579da5a4122b7599bade8b23
Gzoref <gzoref@gmail.com>
2020-06-19 23:38:42 -0400

    doc: add comments

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import POI, Item
 
+'''
+POI link on admin page
+'''
 admin.site.register(POI)
+
+'''
+Item link on admin page
+'''
 admin.site.register(Item)

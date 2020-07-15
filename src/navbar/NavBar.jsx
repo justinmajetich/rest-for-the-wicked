4d6916c5bd019b8fce9ac56fbbf85b6290f14b25
Justin Majetich <justinmajetich@gmail.com>
2020-07-15 17:36:07 -0400

    Polish about page

--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -10,7 +10,7 @@ function NavBar(props) {
                 <p className={props.currentStage === 2 ? "show" : "hide"}>REST for the wicked</p>
             </span>
             <span className={"right-nav-wrapper"}>
-                <a id={"about-link"} href={'/about'}>about</a>
+                <a id={"about-link"} href={'/about'}>/about</a>
                 <a id={"github-link"} href={"https://github.com/justinmajetich/rest-for-the-wicked"}><span>GitHub Page</span></a>
             </span>
         </section>
